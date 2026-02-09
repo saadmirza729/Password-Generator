be52cd0e53327e40c9bb98bf8793e1464225e006
saadmirza729 <saadmirza729@gmail.com>
2026-02-10 00:59:54 +0500

    modification-of-files

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-function generatePassword () {
-
+function generatePassword() {
     let pass = "";
-    let str = "abcdefghijklmnopqrstuvwxyz@#$!123456789"
+    let str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@#$!123456789"; // Added Uppercase too
 
-    for (let i = 0; i < 8; i++) {
-        pass += str[Math.ceil(Math.random()*(str.length -1))]
+    for (let i = 0; i < 12; i++) { // Increased length to 12 for better security
+        // Math.floor is better than Math.ceil to avoid index out of bounds
+        let randomIndex = Math.floor(Math.random() * str.length);
+        pass += str[randomIndex];
     }
-    console.log(pass)
+    
+    console.log("Generated:", pass);
 
-    let pass1 = document.getElementById('pass1')
-    pass1.innerHTML = pass
+    let pass1 = document.getElementById('pass1');
+    pass1.innerHTML = pass;
 }
 
-generatePassword ()
+// Initial call to show a password on load
+generatePassword();
